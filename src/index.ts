e13cc58d331582fc82a4c7f226a5b98c37314610
z89 <n10234977@qut.edu.au>
2021-11-17 23:07:58 +1000

    read file and pass contents to new hamt key value pair

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world!')
+console.time('app');
+
+console.log('starting app...')
+
+const fs = require('fs');
+var hamt = require('hamt');
+
+fs.readFile('./data/picture.png', function read(err: any, data: any) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+    processFile(content);   // Or put the next step in a function and invoke it
+});
+
+function processFile(content: any) {
+    var h = hamt.empty.set('file', content)
+    
+    console.log(h.count())
+    console.log(h.get('file'))
+}
+
+console.timeEnd('app took');
